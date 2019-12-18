d40e6009220eb80604e1fd45e4f0c875bd51665c
Andy Pearson <andy@londonparkour.com>
2019-12-18 11:02:57 +0000

    Update description

--- tb_upcoming_extended.php
+++ tb_upcoming_extended.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: _ANDYP - Team Booking - Custom 'Upcoming' Shortcode
 Plugin URI: https://londonparkour.com
-Description: Custom TeamBooking Upcoming Shortcode - ANDYP - MAKE SURE YOU RUN THE create_symbolic_links.sh script!!!
+Description: Custom TeamBooking Upcoming Shortcode - ANDYP - MAKE SURE YOU RUN THE ./create_tb_links.sh script!!!
 Version: 1.0.0
 Author: Andy Pearson
 Author URI: https://londonparkour.com
